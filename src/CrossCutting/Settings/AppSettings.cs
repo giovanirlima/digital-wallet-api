c36605f52277221caebcca61390a2c1beed040f8
Giovani Rocha Lima <giovani.espressolabs@mastercctvm.com.br>
2025-05-07 23:35:11 -0300

    Adicionado CrossCutting para facilitar leitura de dados do appsettings add .dockerignore

---
+++ src/CrossCutting/Settings/AppSettings.cs
@@ -0,0 +1,13 @@
+﻿using CrossCutting.Settings.Models;
+
+namespace CrossCutting.Settings;
+
+public static class AppSettings
+{
+    public static Database Database { get; set; } = default!;
+
+    public static void Initialize(Database database)
+    {
+        Database = database;
+    }
+}
---
+++ src/CrossCutting/Settings/Models/Database.cs
@@ -0,0 +1,10 @@
+﻿namespace CrossCutting.Settings.Models;
+
+public class Database
+{
+    public string ReadHost { get; set; } = default!;
+    public string WriteHost { get; set; } = default!;
+    public string Base { get; set; } = default!;
+    public string User { get; set; } = default!;
+    public string Password { get; set; } = default!;
+}
